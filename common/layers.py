00c609e36a1cb0828b66a283784ae835e3eed7b1
t-act <takuto.505521@gmail.com>
2026-03-29 22:44:17 +0900

    feat: common/layers.pyにSigmoid・Affineレイヤーの順伝播・逆伝播を実装

--- common/layers.py
+++ common/layers.py
@@ -9,12 +9,52 @@ class MatMul:
     def forward(self, x):
         W, = self.params
         out = np.dot(x, W)
-        self.x = x
+        self.x = x  # backwordで使用するため保存
         return out
 
     def backward(self, dout):
         W, = self.params
         dx = np.dot(dout, W.T)
         dW = np.dot(self.x.T, dout)
+        self.grads[0][...] = dW # parameterはdW
+        return dx   # レイヤー間を逆伝搬していくのはdx
+
+
+class Sigmoid:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+    
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+    
+    def backward(self, dout):
+        # dout -> ∂L/∂y
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+
+    def forward(self, x):
+        W, b = self.params
+        out = np.dot(x, W) + b
+        self.x = x
+        return x
+
+    def backward(self, dout):
+        W, b = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        db = np.sum(dout, axis=0)
+
+        # parameterの保存
         self.grads[0][...] = dW
+        self.grads[1][...] = db
         return dx
