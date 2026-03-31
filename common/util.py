2c48115771db676fd72defe8b2d1a016b50544d3
t-act <takuto.505521@gmail.com>
2026-03-31 22:26:27 +0900

    feat: common.util.pyにpreprocessを実装

--- common/util.py
+++ common/util.py
@@ -10,3 +10,20 @@ def clip_grads(grads, max_norm):
     if rate < 1:
         for grad in grads:
             grad *= rate
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace(".", " .")
+    words = text.split(" ")
+
+    word_to_id = {}
+    id_to_word = {}
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+    
+    corpus = np.array([word_to_id[w] for w in words])
+
+    return corpus, word_to_id, id_to_word
