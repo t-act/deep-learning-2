21b593f27502ca500ebbb07de1ac0d549d415f51
t-act <takuto.505521@gmail.com>
2026-03-31 16:26:55 +0900

    update: spiral datasetを可視化

--- ch01/show_spiral_dataset.py
+++ ch01/show_spiral_dataset.py
@@ -1,4 +1,13 @@
-import sys
-sys.path.append("..")
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from dataset import spiral
 import matplotlib.pyplot as plt
+
+x, t = spiral.load_data()
+t_label = t.argmax(axis=1)
+
+for cls in range(3):
+    mask = t_label == cls
+    plt.scatter(x[mask, 0], x[mask, 1], label=f'class {cls}')
+
+plt.show()
