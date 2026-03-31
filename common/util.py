ab00738c1f1dfc7e6f52c26c3e2f82d213746ad7
t-act <takuto.505521@gmail.com>
2026-03-31 22:42:55 +0900

    common.util.pyにcos_similaryを実装

--- common/util.py
+++ common/util.py
@@ -36,7 +36,7 @@ def create_co_matrix(corpus, vocab_size, window_size=1):
         for i in range(1, window_size+1):
             left_idx = idx - i
             right_idx = idx + i
-            
+
             if left_idx >= 0:
                 left_word_id = corpus[left_idx]
                 co_matrix[word_id, left_word_id] += 1
@@ -46,3 +46,8 @@ def create_co_matrix(corpus, vocab_size, window_size=1):
                 co_matrix[word_id, right_word_id] += 1
 
     return co_matrix
+
+def cos_similarity(x, y, eps=1e-8):
+    nx = x / np.sqrt(np.sum(x**2) + eps)
+    ny = y / np.sqrt(np.sum(y**2) + eps)
+    return np.dot(nx, ny)
