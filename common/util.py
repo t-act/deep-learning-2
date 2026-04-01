f55748ac87b94a1718ce497b81ff528748863782
t-act <takuto.505521@gmail.com>
2026-04-01 09:29:49 +0900

    feat: common.util.pyにppmiを実装

--- common/util.py
+++ common/util.py
@@ -89,3 +89,29 @@ def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
         count += 1
         if count >= top:
             return
+        
+def ppmi(C, verbose=False, eps=1e-8):
+    '''PPMI（正の相互情報量）の作成
+
+    :param C: 共起行列
+    :param verbose: 進行状況を出力するかどうか
+    :return M: PPMI行列（正の相互情報量）
+    '''
+
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    total = C.shape[0] * C.shape[1]
+    count = 0
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i,j] * N / (S[j]*S[i]) + eps)
+            M[i,j] = max(0, pmi)
+
+            if verbose:
+                count += 1
+                if count % (total//100 + 1) == 0:
+                    print('%.1f%% done' % (100*count/total))
+
+    return M
