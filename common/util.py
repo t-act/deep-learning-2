1e1b9fe6ba317bf3f632dd2081e42144fdf564bc
t-act <takuto.505521@gmail.com>
2026-04-01 23:18:54 +0900

    feat: common.utilにconvert_one_hotをコピー

--- common/util.py
+++ common/util.py
@@ -129,3 +129,26 @@ def create_contexts_target(corpus, window_size=1):
         contexts.append(cs)
 
     return np.array(contexts), np.array(target)
+
+def convert_one_hot(corpus, vocab_size):
+    '''one-hot表現への変換
+
+    :param corpus: 単語IDのリスト（1次元もしくは2次元のNumPy配列）
+    :param vocab_size: 語彙数
+    :return: one-hot表現（2次元もしくは3次元のNumPy配列）
+    '''
+    N = corpus.shape[0]
+
+    if corpus.ndim == 1:
+        one_hot = np.zeros((N, vocab_size), dtype=np.int32)
+        for idx, word_id in enumerate(corpus):
+            one_hot[idx, word_id] = 1
+
+    elif corpus.ndim == 2:
+        C = corpus.shape[1]
+        one_hot = np.zeros((N, C, vocab_size), dtype=np.int32)
+        for idx_0, word_ids in enumerate(corpus):
+            for idx_1, word_id in enumerate(word_ids):
+                one_hot[idx_0, idx_1, word_id] = 1
+
+    return one_hot
