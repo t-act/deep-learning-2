51781c2ee3c43a288b5c1a1bd67a98f5e31d503d
t-act <takuto.505521@gmail.com>
2026-04-01 09:12:58 +0900

    feat: common.util.pyに most_similarを実装

--- common/util.py
+++ common/util.py
@@ -51,3 +51,41 @@ def cos_similarity(x, y, eps=1e-8):
     nx = x / np.sqrt(np.sum(x**2) + eps)
     ny = y / np.sqrt(np.sum(y**2) + eps)
     return np.dot(nx, ny)
+
+def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    '''類似単語の検索
+
+    :param query: クエリ（テキスト）
+    :param word_to_id: 単語から単語IDへのディクショナリ
+    :param id_to_word: 単語IDから単語へのディクショナリ
+    :param word_matrix: 単語ベクトルをまとめた行列。各行に対応する単語のベクトルが格納されていることを想定する
+    :param top: 上位何位まで表示するか
+    '''
+
+    # 1. クエリを取り出す
+    if query not in word_to_id:
+        print(f"{query} is not found")
+        return
+    print(f"[query] = {query}")
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+
+    # 2. コサイン類似度の算出
+    vocab_size = len(id_to_word)
+    similarity = np.zeros(vocab_size)
+    # 特定のqueryベクトル vs 全単語(word_matrix)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(word_matrix[i], query_vec)
+    
+    # 3. コサイン類似度の結果から、その値を高い順に出力
+    count = 0
+    # argsort()は降順にするために -1をかける
+    for i in (-1 * similarity).argsort():
+        # 全単語(word_matrix)の中にある特定のqueryベクトルは除く
+        if id_to_word[i] == query:
+            continue
+        print(f"{id_to_word[i]}: {similarity[i]}")
+
+        count += 1
+        if count >= top:
+            return
