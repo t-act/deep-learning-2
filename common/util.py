792f0fa9fe71064956be1e52dcc30528fe67710e
t-act <takuto.505521@gmail.com>
2026-04-08 20:13:45 +0900

    copy: common.util.pyにeval_perplexityをコピー

--- common/util.py
+++ common/util.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 def clip_grads(grads, max_norm):
     total_norm = 0
@@ -152,3 +153,33 @@ def convert_one_hot(corpus, vocab_size):
                 one_hot[idx_0, idx_1, word_id] = 1
 
     return one_hot
+
+def eval_perplexity(model, corpus, batch_size=10, time_size=35):
+    print('evaluating perplexity ...')
+    corpus_size = len(corpus)
+    total_loss = 0
+    max_iters = (corpus_size - 1) // (batch_size * time_size)
+    jump = (corpus_size - 1) // batch_size
+
+    for iters in range(max_iters):
+        xs = np.zeros((batch_size, time_size), dtype=np.int32)
+        ts = np.zeros((batch_size, time_size), dtype=np.int32)
+        time_offset = iters * time_size
+        offsets = [time_offset + (i * jump) for i in range(batch_size)]
+        for t in range(time_size):
+            for i, offset in enumerate(offsets):
+                xs[i, t] = corpus[(offset + t) % corpus_size]
+                ts[i, t] = corpus[(offset + t + 1) % corpus_size]
+
+        try:
+            loss = model.forward(xs, ts, train_flg=False)
+        except TypeError:
+            loss = model.forward(xs, ts)
+        total_loss += loss
+
+        sys.stdout.write('\r%d / %d' % (iters, max_iters))
+        sys.stdout.flush()
+
+    print('')
+    ppl = np.exp(total_loss / max_iters)
+    return ppl
