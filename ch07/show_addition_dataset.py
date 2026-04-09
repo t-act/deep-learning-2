840085e50409855bb16d882e0c009b54ba1c5a1b
t-act <takuto.505521@gmail.com>
2026-04-09 22:48:08 +0900

    feat: ch07/show_addition_dataset.pyを実装

---
+++ ch07/show_addition_dataset.py
@@ -0,0 +1,16 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from dataset import sequence
+
+(x_train, t_train), (x_test, t_test) = sequence.load_data("addition.txt", seed=1984)
+char_to_id, id_to_char = sequence.get_vocab()
+
+print(x_train.shape, t_train.shape)
+print(x_test.shape, t_test.shape)
+
+print(x_train[0])
+print(t_train[0])
+print(id_to_char)
+
+print(''.join([id_to_char[c] for c in x_train[0]]))
+print(''.join([id_to_char[c] for c in t_train[0]]))
