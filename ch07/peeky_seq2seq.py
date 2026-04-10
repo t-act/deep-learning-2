f9bd9c4f4c5dd133458e37b159d8b3f2d9baba76
t-act <takuto.505521@gmail.com>
2026-04-11 01:13:35 +0900

    feat: ch07/peeky_seq2seq.pyを実装

---
+++ ch07/peeky_seq2seq.py
@@ -0,0 +1,90 @@
+import numpy as np
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from common.time_layers import *
+from common.base_model import BaseModel
+from seq2seq import *
+
+class PeekyDecoder:
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+
+        embed_W = (rn(V, D) / 100).astype("f")
+        lstm_Wx = (rn(H + D, 4 * H) / np.sqrt(D)).astype("f")
+        lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype("f")
+        lstm_b = np.zeros(4 * H).astype("f")
+        affine_W = (rn(H + H, V) / np.sqrt(H)).astype("f")
+        affine_b = np.zeros(V).astype("f")
+
+        self.embed = TimeEmbedding(embed_W)
+        self.lstm = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=True)
+        self.affine = TimeAffine(affine_W, affine_b)
+
+        self.params, self.grads = [], []
+        for layer in (self.embed, self.lstm, self.affine):
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def forward(self, xs, h):
+        N, T = xs.shape
+        N, H = h.shape
+
+        self.lstm.set_state(h)
+
+        out = self.embed.forward(xs)
+        hs = np.repeat(h, T, axis=0).reshape(N, T, H)  # (N, H) → (N, T, H) に拡張
+        out = np.concatenate((hs, out), axis=2)  # (N, T, H+D)
+
+        out = self.lstm.forward(out)
+        out = np.concatenate((hs, out), axis=2)  # (N, T, H+H)
+
+        score = self.affine.forward(out)
+        return score
+    
+    # @copied
+    def backward(self, dscore):
+        H = self.cache
+
+        dout = self.affine.backward(dscore)
+        dout, dhs0 = dout[:, :, H:], dout[:, :, :H]
+        dout = self.lstm.backward(dout)
+        dembed, dhs1 = dout[:, :, H:], dout[:, :, :H]
+        self.embed.backward(dembed)
+
+        dhs = dhs0 + dhs1
+        dh = self.lstm.dh + np.sum(dhs, axis=1)
+        return dh
+
+    # @copied
+    def generate(self, h, start_id, sample_size):
+        sampled = []
+        char_id = start_id
+        self.lstm.set_state(h)
+
+        H = h.shape[1]
+        peeky_h = h.reshape(1, 1, H)
+        for _ in range(sample_size):
+            x = np.array([char_id]).reshape((1, 1))
+            out = self.embed.forward(x)
+
+            out = np.concatenate((peeky_h, out), axis=2)
+            out = self.lstm.forward(out)
+            out = np.concatenate((peeky_h, out), axis=2)
+            score = self.affine.forward(out)
+
+            char_id = np.argmax(score.flatten())
+            sampled.append(char_id)
+
+        return sampled
+
+# @copied
+class PeekySeq2seq(Seq2seq):
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        self.encoder = Encoder(V, D, H)
+        self.decoder = PeekyDecoder(V, D, H)
+        self.softmax = TimeSoftmaxWithLoss()
+
+        self.params = self.encoder.params + self.decoder.params
+        self.grads = self.encoder.grads + self.decoder.grads
