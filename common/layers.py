3df10c59c8ac70f27bbe46cd6c6231b4ad4be133
t-act <takuto.505521@gmail.com>
2026-04-11 10:03:10 +0900

    copy: common.layers.pyにSoftmaxクラスをコピー

--- common/layers.py
+++ common/layers.py
@@ -128,3 +128,19 @@ class SigmoidWithLoss:
 
         dx = (self.y - self.t) * dout / batch_size
         return dx
+
+
+class Softmax:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+
+    def forward(self, x):
+        self.out = softmax(x)
+        return self.out
+
+    def backward(self, dout):
+        dx = self.out * dout
+        sumdx = np.sum(dx, axis=1, keepdims=True)
+        dx -= self.out * sumdx
+        return dx
