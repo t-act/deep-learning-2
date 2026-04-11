961bcb714c786eca164c69fb72490c2c3e12cd72
t-act <takuto.505521@gmail.com>
2026-04-11 10:27:22 +0900

    feat: ch08/attention_layer.pyにAttentionクラスを実装

--- ch08/attention_layer.py
+++ ch08/attention_layer.py
@@ -58,3 +58,23 @@ class AttentionWeight:
         dh = np.sum(dhr, axis=1)
 
         return dhs, dh
+
+
+class Attention:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.attention_weight_layer = AttentionWeight()
+        self.weight_sum_layer = WeightSum()
+        self.attention_weight = None
+    
+    def forward(self, hs, h):
+        a = self.attention_weight_layer.forward(hs, h)
+        out = self.weight_sum_layer.forward(hs, a)
+        self.attention_weight = a
+        return out
+    
+    def backward(self, dout):
+        dhs0, da = self.weight_sum_layer.backward(dout)
+        dhs1, dh = self.attention_weight_layer.backward(da)
+        dhs = dhs0 + dhs1
+        return dhs, dh
