1ae366b8730de4be664f27fdaf9515038ea588e6
t-act <takuto.505521@gmail.com>
2026-04-11 10:10:13 +0900

    feat: ch08/attention_layer.pyにAttentionWeightクラスを実装

--- ch08/attention_layer.py
+++ ch08/attention_layer.py
@@ -1,4 +1,7 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import numpy as np
+from common.layers import Softmax
 
 class WeightSum:
     def __init__(self):
@@ -25,3 +28,33 @@ class WeightSum:
         da = np.sum(dar, axis=2)  # repeatの逆伝搬
 
         return dhs, da
+    
+
+class AttentionWeight:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.softmax = Softmax()
+        self.cache = None
+    
+    def forward(self, hs, h):
+        N, T, H = hs.shape
+
+        hr = h.reshape(N, 1, H).repeat(T, axis=1)
+        t = hs * hr
+        s = np.sum(t, axis=2)
+        a = self.softmax(s)  # 重みをsoftmax関数で正規化
+
+        self.cache = (hs, hr)
+        return a
+    
+    def backward(self, da):
+        hs, hr = self.cache
+        N, T, H = hs.shape
+
+        ds = self.softmax.backward(da)
+        dt = ds.reshape(N, T, 1).repeat(H, axis=2)  # sumのbackward
+        dhs = dt * hr
+        dhr = dt * hs
+        dh = np.sum(dhr, axis=1)
+
+        return dhs, dh
