0f90be172853f56408e2e4cf9299c552480853a9
t-act <takuto.505521@gmail.com>
2026-04-13 22:25:56 +0900

    exp: ch08/exp_train_add.pyを実装（additionデータでAttentionSeq2seqを実験）

---
+++ ch08/exp_train_add.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from common.time_layers import *
+from ch07.seq2seq import Encoder, Seq2seq
+from ch08.attention_layer import TimeAttention
+from dataset import sequence
+from common.optimizer import Adam
+from common.trainer import Trainer
+from common.util import eval_seq2seq
+from attention_seq2seq import AttentionSeq2seq
+from ch07.seq2seq import Seq2seq
+from ch07.peeky_seq2seq import PeekySeq2seq
+
+# データの読み込み
+(x_train, t_train), (x_test, t_test) = sequence.load_data("addition.txt")
+char_to_id, id_to_char = sequence.get_vocab()
+
+# 入力文を反転
+x_train, x_test = x_train[:, ::-1], x_test[:, ::-1]
+
+# ハイパーパラメータの設定
+vocab_size = len(char_to_id)
+wordvec_size = 16
+hidden_size = 128
+batch_size = 128
+max_epoch = 25
+max_grad = 5.0
+
+model = AttentionSeq2seq(vocab_size, wordvec_size, hidden_size)
+optimizer = Adam()
+trainer = Trainer(model, optimizer)
+
+acc_list = []
+for epoch in range(max_epoch):
+    trainer.fit(x_train, t_train, max_epoch=1, batch_size=batch_size, max_grad=max_grad)
+    correct_num = 0
+    for i in range(len(x_test)):
+        question, correct = x_test[[i]], t_test[[i]]
+        verbose = i < 10
+        correct_num += eval_seq2seq(model, question, correct, id_to_char, verbose, is_reverse=True)
+    
+    acc = float(correct_num) / len(x_test)
+    acc_list.append((epoch, acc))
+    print(f"val acc: {acc*100:.3f}")
+
+model.save_params()
+
+# save
+acc_list = np.array(acc_list)
+file_name = f"acc_attention.npy"
+file_path = os.path.join(os.path.dirname(__file__), file_name)
+np.save(file_path, acc_list)
+print(f"saved: {file_name}")
